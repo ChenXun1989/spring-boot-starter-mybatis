38144ab098119895314fc0e9b7b09fb959483e67
chenxun <576481228@qq.com>
2016-08-19 14:00:58 +0800

    add class

---
+++ src/main/java/com/midai/springboot/mybatis/Test.java
@@ -0,0 +1,34 @@
+/**
+ * Project Name:spring-boot-starter-mybatis
+ * File Name:Test.java
+ * Package Name:com.midai.springboot.mybatis
+ * Date:2016年7月27日下午6:04:20
+ * Copyright (c) 2016, www midaigroup com Technology Co., Ltd. All Rights Reserved.
+ *
+*/
+
+package com.midai.springboot.mybatis;
+
+
+
+/**
+ * ClassName:Test <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2016年7月27日 下午6:04:20 <br/>
+ * @author   陈勋
+ * @version  
+ * @since    JDK 1.7
+ * @see 	 
+ */
+
+public class Test {
+
+	public static void main(String[] args) {
+
+	
+
+	}
+
+}
+
